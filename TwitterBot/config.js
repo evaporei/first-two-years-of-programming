502c653f7650eb6f7ee8c217f834168b28893854
Otávio Pace <otaviopp8@gmail.com>
2018-09-25 23:16:22 -0300

    project: add TwitterBot

---
+++ TwitterBot/config.js
@@ -0,0 +1,8 @@
+//objeto que será importado no objeto Twitter
+
+module.exports = {
+	consumer_key:        'REDACTED',
+	consumer_secret:     'REDACTED',
+	access_token:        'REDACTED',
+	access_token_secret: 'REDACTED'
+}
